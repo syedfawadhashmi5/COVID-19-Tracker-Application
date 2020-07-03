93a01eb264ba244a9a9b9c279d1f825192112b51
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-07-03 04:29:14 -0700

    create cards and update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Cards, Chart, CountryPicker } from './components';
+import { getData } from './Api'
 
 import styles from './App.module.css' 
 
-console.log(Cards)
-function App() {
-  return (
-    <div className={styles.container}>
-      <Cards />
-
-    </div>
-  );
+export class App extends Component {
+  state = {
+    data: {}
+  }
+  async componentDidMount() {
+      const fetchapi = await getData();
+      this.setState({data: fetchapi})
+    }
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/Card/Cards.jsx
+++ src/components/Card/Cards.jsx
@@ -1,37 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import cx from 'classnames';
+import CountUp from 'react-countup';
+import styles from './Card.module.css';
 
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if (!confirmed) {
+        return <div className='lds-hourglass'></div>
+    }
 
-import cx from 'classnames'
-import styles from './Card.module.css'
+    // const [globalData, setGlobalData] = useState();
 
-function Cards() {
+    // useEffect( () => {
+
+    //     async function  fetchGlobalData() {
+    //         const apiresponse = await fetch('https://api.thevirustracker.com/free-api?global=stats');
+    //         const response = await apiresponse.json();
+    //         console.log(response)
+    //         setGlobalData(response)
+    //     }
+// this is secoend methot of api call and use {globalData && globalData.results && globalData.results[0].total_recovered}
+    // fetchGlobalData();
+    // }, []);
+
     return (
-
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card} sm={12} md={3} className={cx (styles.card, styles.Infected)}>
+                <Grid item component={Card} sm={12} md={3} className={cx(styles.card, styles.Infected)}>
                     <CardContent>
-                        <Typography color='textSecondary' guterbottem>Infected</Typography>
-                        <Typography varaint="h5">Real data</Typography>
-                        <Typography color='textSecondary'>Real data</Typography>
+                        <Typography className={styles.Infect}>Infected</Typography>
+                        <Typography varaint="h5">
+                        <CountUp start={0} end={confirmed.value} duration={3.5}  separator=',' />
+                        </Typography>
+                        <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography varaint="body2">Number of Active Cases of COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card} sm={12} md={3} className={cx (styles.card, styles.Deaths)}>
-                    <CardContent>
-                        <Typography color='textSecondary' guterbottem>Deaths</Typography>
-                        <Typography varaint="h5">Real data</Typography>
-                        <Typography color='textSecondary'>Real data</Typography>
-                        <Typography varaint="body2">Number of Deaths Cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} sm={12} md={3} className={cx (styles.card, styles.Recoveries)}>
                     <CardContent>
-                        <Typography color='textSecondary' guterbottem>Recoveries</Typography>
-                        <Typography varaint="h5">Real data</Typography>
-                        <Typography color='textSecondary'>Real data</Typography>
+                        <Typography className={styles.Recover}>Recoveries</Typography>
+                        <Typography varaint="h5">
+                        <CountUp start={0} end={recovered.value} duration={3.5}  separator=',' />
+                        </Typography>
+                        <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography varaint="body2">Number of Recoveries of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card} sm={12} md={3} className={cx (styles.card, styles.Deaths)}>
+                    <CardContent>
+                        <Typography className={styles.Death}>Deaths</Typography>
+                        <Typography varaint="h5">
+                        <CountUp start={0} end={deaths.value} duration={3.5}  separator=',' />
+                        </Typography>
+                        <Typography color='textSecondary'>{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography varaint="body2">Number of Deaths Cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
