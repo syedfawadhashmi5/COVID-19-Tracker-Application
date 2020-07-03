f4b832b99f5da086f93e69e9f5cfd666a9411296
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-07-03 04:44:21 -0700

    add and remove
    add Chart and CountryPicker Component in app.js,
    
    remove useState and useEffect in Cards Component

--- src/components/Card/Cards.jsx
+++ src/components/Card/Cards.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import cx from 'classnames';
 import CountUp from 'react-countup';
